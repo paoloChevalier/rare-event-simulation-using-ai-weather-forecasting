0c7606c396fb58cbdb99993f0974f0016ef842dc
Paolo <paoloo.chevalier@gmail.com>
2026-03-30 16:15:32 +0200

    Refactor run_aifs.py for improved structure and clarity
    allows for multiple fields or scales in one experiment

--- scripts/run_aifs.py
+++ scripts/run_aifs.py
@@ -2,9 +2,13 @@
 import datetime
 from collections import defaultdict
 import os
+
+os.environ["PYTHONHASHSEED"] = "14012003"
+
 # if usage is too expensive on ram
-#os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'expandable_segments:True' 
-#os.environ['ANEMOI_INFERENCE_NUM_CHUNKS'] = '16'
+os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'expandable_segments:True' 
+os.environ['ANEMOI_INFERENCE_NUM_CHUNKS'] = '16'
+
 import pickle
 import glob
 import argparse
@@ -18,10 +22,13 @@ import random
 
 def set_seed(seed):
     torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
     random.seed(seed)
     np.random.seed(seed)
+    
 
 def set_deterministic():
+    os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
     torch.backends.cudnn.benchmark = False
     torch.backends.cudnn.deterministic = True
     torch.use_deterministic_algorithms(True)
@@ -49,47 +56,46 @@ def run_aifs(input_path, output_path, date_str, exp_name, checkpoint, lead_time=
     if not found_dirs:
         raise FileNotFoundError(f"No input directory found matching: {dir_pattern}")
 
-    #get one of the files
-    in_dir = found_dirs[0]
-    dir_name = os.path.basename(in_dir) # This captures the full string: "2025062500-loremipsum-uniform-1e-04-q"
-
-    #creates the output directry if it doesn't exist
-    out_dir = os.path.join(output_path, dir_name)
-    os.makedirs(out_dir, exist_ok=True)
-    
     #loading the model
     runner = SimpleRunner(checkpoint, device="cuda")
 
-    #for each member, load the initial state, run the model and save the output
-    for i in range(N_MEMBERS + 1):
-        # format the member id to be two digits, e.g. 00, 01, ..., 50 and construct the input filename
-        member_id = f"{i:02d}"
-        input_filename = os.path.join(in_dir, f"init_state-{dir_name}-{member_id}.pkl")
-        # load the initial state from the input file
-        with open(input_filename,"rb") as f:
-            input_state = pickle.load(f)
+    for in_dir in found_dirs:
+        dir_name = os.path.basename(in_dir) # This captures the full string: "2025062500-loremipsum-uniform-1e-04-q"
 
-        # run the model and collect the forecast steps
-        forecast_steps = []   
-        #save initial state as the first step of the forecast
-        forecast_steps.append({
-            "date": input_state["date"],
-            "fields": {k: v[1] for k, v in input_state["fields"].items() if k in target_variables}
-        })
-        #forecast
-        for state in runner.run(input_state=input_state, lead_time=lead_time):
-            # filter the state to only include the target variables
-            if state["date"].hour in times_to_save:
-                filtered_state = {
-                    "date": state["date"],
-                    "fields": {k: v for k, v in state["fields"].items() if k in target_variables}
-                }
-                forecast_steps.append(filtered_state)
-        
-        # save the forecast steps to the output file
-        output_filename = os.path.join(out_dir, f"aifs_output-{dir_name}-{member_id}.pkl")
-        with open(output_filename, "wb") as f_out:
-            pickle.dump(forecast_steps, f_out)
+        #creates the output directry if it doesn't exist
+        out_dir = os.path.join(output_path, dir_name)
+        os.makedirs(out_dir, exist_ok=True)
+    
+        #for each member, load the initial state, run the model and save the output
+        for i in range(N_MEMBERS + 1):
+            # format the member id to be two digits, e.g. 00, 01, ..., 50 and construct the input filename
+            member_id = f"{i:02d}"
+            input_filename = os.path.join(in_dir, f"init_state-{dir_name}-{member_id}.pkl")
+            # load the initial state from the input file
+            with open(input_filename,"rb") as f:
+                input_state = pickle.load(f)
+
+            # run the model and collect the forecast steps
+            forecast_steps = []   
+            #save initial state as the first step of the forecast
+            forecast_steps.append({
+                "date": input_state["date"],
+                "fields": {k: v[1] for k, v in input_state["fields"].items() if k in target_variables}
+            })
+            #forecast
+            for state in runner.run(input_state=input_state, lead_time=lead_time):
+                # filter the state to only include the target variables
+                if state["date"].hour in times_to_save:
+                    filtered_state = {
+                        "date": state["date"],
+                        "fields": {k: v for k, v in state["fields"].items() if k in target_variables}
+                    }
+                    forecast_steps.append(filtered_state)
+            
+            # save the forecast steps to the output file
+            output_filename = os.path.join(out_dir, f"aifs_output-{dir_name}-{member_id}.pkl")
+            with open(output_filename, "wb") as f_out:
+                pickle.dump(forecast_steps, f_out)
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -106,6 +112,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     set_seed(args.seed)
+    set_deterministic()
     run_aifs(
         args.input_path, args.output_path, args.date_str, args.exp_name, 
         args.checkpoint, args.lead_time, args.variables, args.savetimes, args.n_members
