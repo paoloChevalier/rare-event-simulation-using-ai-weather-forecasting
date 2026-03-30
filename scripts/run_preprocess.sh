be798bf3838357d19cc442ce36669888f42c1f76
Paolo <paoloo.chevalier@gmail.com>
2026-03-30 15:37:08 +0200

    Add files via upload

---
+++ scripts/run_preprocess.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# ==========================================
+#              SBATCH SETTINGS
+# ==========================================
+
+#SBATCH --partition=zen4
+#SBATCH --time=1:0:00
+#SBATCH --mem=32G
+
+# ==========================================
+#              PREPARE ENV
+# ==========================================
+
+module purge
+module load pangeo-meso/2026.01.21
+
+conda activate /scratchx/pchevali/python_envs/pangeo-earthkit 
+
+# ==========================================
+#          EXPERIMENT PARAMETERS
+# ==========================================
+
+EXP_NAME="test_perturbation_deterministic"
+DATE_INPUT="2025-06-25 00:00:00"
+
+PERT_TYPE="uniform"
+N_MEMBERS=5
+#commas mean together and spaces separate experiments
+SCALES="1e-3 1e-5 1e-7 1e-9"
+# for example this will run 3 experiments: 'q' alone, 'u' and 'v' together, and all three together
+PERT_FIELDS="q u,v q,u,v"
+
+INPUTS_SAVE="/homedata/pchevali/AIFS_INPUTS/"
+
+# ==========================================
+#              RUN PREPROCESS
+# ==========================================
+
+python /home/pchevali/Stage2026/scripts_aifs_ens_boosting/aifs_inputs_dl+preprocess+perturbation.py \
+    --date_input "$DATE_INPUT" \
+    --n_members $N_MEMBERS \
+    --perturbed_fields $PERT_FIELDS \
+    --scales $SCALES \
+    --exp_name "$EXP_NAME" \
+    --pert_type "$PERT_TYPE" \
+    --output_save "$INPUTS_SAVE"
