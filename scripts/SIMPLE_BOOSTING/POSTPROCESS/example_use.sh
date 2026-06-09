e689b64691997910fe8f33533767d30678393bcf
Paolo <paoloo.chevalier@gmail.com>
2026-06-09 10:09:08 +0200

    Add mask parameter to postprocess script call

--- scripts/SIMPLE_BOOSTING/POSTPROCESS/example_use.sh
+++ scripts/SIMPLE_BOOSTING/POSTPROCESS/example_use.sh
@@ -26,14 +26,17 @@ N_MEMBERS=128
 
 REGRID_IN_DIR="/scratchx/pchevali/AIFS_OUTPUTS/"
 REGRID_OUT_DIR="/scratchx/pchevali/AIFS_OUTPUTS_REGRIDDED/"
+MASK="/homedata/pchevali/mask_eu.pkl"
+
 
 # ==========================================
 #              RUN POSTPROCESS
 # ==========================================
 
-python simple_boosting_postprocess.sh.py \
+python aifs_outputs_process_crps.py \
     --input_dir "$REGRID_IN_DIR" \
     --output_dir "$REGRID_OUT_DIR" \
     --date_str "$DATE_STR" \
     --exp_name "$EXP_NAME" \
-    --n_members $N_MEMBERS
+    --n_members $N_MEMBERS \
+    --mask "$MASK"
